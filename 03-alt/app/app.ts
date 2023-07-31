3583afc13daa0c4a61a60f433a19cbb8d9a85d4d
Garry Trinder <garry@trinder365.co.uk>
2023-07-31 16:28:55 +0100

    Bring task module code into bot

--- 03-alt/app/app.ts
+++ 03-alt/app/app.ts
@@ -4,7 +4,6 @@ import adapter from '../adapter';
 import config from '../config';
 import * as bot from './bot/bot';
 import * as me from './me/me';
-import * as tm from './tm/tm';
 
 interface ConversationState {
     count: number;
@@ -27,6 +26,5 @@ const app = new Application<ApplicationTurnState>({
 // Setup bot and messaging extensions
 bot.setup(app);
 me.setup(app);
-tm.setup(app);
 
 export default app;
--- 03-alt/app/bot/bot.ts
+++ 03-alt/app/bot/bot.ts
@@ -1,8 +1,23 @@
-import { ActivityTypes, TurnContext } from "botbuilder";
+import { ActivityTypes, TaskModuleTaskInfo, TurnContext } from "botbuilder";
 import { Application } from "@microsoft/teams-ai";
 import { ApplicationTurnState } from "../app";
 
 const setup = (app: Application) => {
+
+    // add installation update handler to welcome user
+    app.activity(ActivityTypes.InstallationUpdate, async (context: TurnContext, state: ApplicationTurnState) => {
+        await context.sendActivity(`Thanks for installing me!`);
+    });
+
+    // add message handler to echo back user input
+    app.activity(ActivityTypes.Message, async (context: TurnContext, state: ApplicationTurnState) => {
+        // Increment count state
+        let count = state.conversation.value.count ?? 0;
+        state.conversation.value.count = ++count;
+
+        // Echo back users request
+        await context.sendActivity(`[${count}] you said: ${context.activity.text}`);
+    });
 
     // add reset command to reset conversation state
     app.message('/reset', async (context: TurnContext, state: ApplicationTurnState) => {
@@ -44,19 +59,59 @@ const setup = (app: Application) => {
         });
     });
 
-    // add message handler to echo back user input
-    app.activity(ActivityTypes.Message, async (context: TurnContext, state: ApplicationTurnState) => {
-        // Increment count state
-        let count = state.conversation.value.count ?? 0;
-        state.conversation.value.count = ++count;
-
-        // Echo back users request
-        await context.sendActivity(`[${count}] you said: ${context.activity.text}`);
+    // handle the task/fetch invoke event 
+    app.taskModules.fetch('fetchName', async (context: TurnContext, state: ApplicationTurnState, data: Record<string, any>) => {
+        return {
+            type: 'continue',
+            title: 'Task Module',
+            card: {
+                contentType: 'application/vnd.microsoft.card.adaptive',
+                content: {
+                    "type": "AdaptiveCard",
+                    "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
+                    "version": "1.5",
+                    "body": [
+                        {
+                            "type": "Input.Text",
+                            "id": "name",
+                            "text": "Your name",
+                            "placeholder": "Your name",
+                        }
+                    ],
+                    "actions": [
+                        {
+                            "type": "Action.Submit",
+                            "title": "Submit",
+                            data: {
+                                verb: 'submitName',
+                            }
+                        }
+                    ]
+                }
+            }
+        } as TaskModuleTaskInfo;
     });
 
-    // add installation update handler to welcome user
-    app.activity(ActivityTypes.InstallationUpdate, async (context: TurnContext, state: ApplicationTurnState) => {
-        await context.sendActivity(`Thanks for installing me!`);
+    // handle the task/submit invoke event
+    app.taskModules.submit('submitName', async (context: TurnContext, state: ApplicationTurnState, data: Record<string, any>) => {
+        await context.sendActivity(`Hello, ${data.name}!`);
+        return {
+            type: 'continue',
+            card: {
+                contentType: 'application/vnd.microsoft.card.adaptive',
+                content: {
+                    "type": "AdaptiveCard",
+                    "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
+                    "version": "1.5",
+                    "body": [
+                        {
+                            "type": "TextBlock",
+                            "text": `Hello, ${data.name}!`,
+                        }
+                    ],
+                }
+            }
+        } as TaskModuleTaskInfo;
     });
 }
 
