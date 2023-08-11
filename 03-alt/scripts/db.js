74fc3c14e160c33f11afd2b38a903484948ae1ae
Bob German <Bob.German@microsoft.com>
2023-08-11 14:18:01 -0700

    DB now uses Northwind primary keys as rowkey

--- 03-alt/scripts/db.js
+++ 03-alt/scripts/db.js
@@ -7,15 +7,18 @@ const path = require("path");
 
     const tableServiceClient = TableServiceClient.fromConnectionString("UseDevelopmentStorage=true");
     const tables = ["Categories", "Customers", "Employees", "Orders", "OrderDetails", "Products", "Regions", "Suppliers", "Territories"];
+    const rowKeyColumnNames = ["CategoryID", "CustomerID", "EmployeeID", "OrderID", null, "ProductID", "RegionID", "SupplierID", "TerritoryID"];
+
     tables.forEach(async (table) => await tableServiceClient.deleteTable(table));
     tables.forEach(async (table) => {
+        const rowKeyColumnName = rowKeyColumnNames[tables.indexOf(table)];
         await tableServiceClient.createTable(table);
         const tableClient = TableClient.fromConnectionString("UseDevelopmentStorage=true", table);
         const jsonString = fs.readFileSync(path.resolve(__dirname, "db", `${table}.json`), "utf8");
         const entities = JSON.parse(jsonString);
         for (const entity of entities[table]) {
-            const rowKey = randomUUID();
-            console.log(`${table} - ${rowKey}`);
+            const rowKey = rowKeyColumnName ? entity[rowKeyColumnName].toString() : randomUUID();
+            console.log(`Added ${table} - ${rowKey}`);
             await tableClient.createEntity({
                 partitionKey: table,
                 rowKey,
