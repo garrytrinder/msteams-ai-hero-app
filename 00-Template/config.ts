0073b6c3212ee40aa4afa878449cf22be0a91894
Garry Trinder <garry@trinder365.co.uk>
2023-08-30 10:59:37 +0100

    Remove env vars that are not required (#10)

--- 00-Template/config.ts
+++ 00-Template/config.ts
@@ -3,8 +3,7 @@ const config = {
   aadAppClientSecret: process.env.AAD_APP_CLIENT_SECRET,
   appEndpoint: process.env.APP_ENDPOINT,
   blobConnectionString: process.env.BLOB_STORAGE_CONNECTION_STRING,
-  blobContainerName: process.env.BLOB_STORAGE_CONTAINER_NAME,
-  tableConnectionString: process.env.TABLE_STORAGE_CONNECTION_STRING,
+  blobContainerName: process.env.BLOB_STORAGE_CONTAINER_NAME
 };
 
 export default config;
