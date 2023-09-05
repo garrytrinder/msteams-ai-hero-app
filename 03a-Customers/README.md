679b0de3b1ba156f647ed1c775f24a62c73e3658
Bob German <Bob.German@microsoft.com>
2023-09-05 16:21:52 -0400

    Copy 01 --> 03a

---
+++ 03a-Customers/README.md
@@ -0,0 +1,11 @@
+# Path to awesome
+
+1. Install Azure data tables, used to hold Northwind data
+
+`npm install @azure/data-tables`
+
+1. While Azurite is running (app running locally)
+
+`node scripts/db.js`
+
+to load the Northwind data into Azurite.
---
+++ 03a-Customers/app/messageExtensions/messageExtensions.ts
@@ -0,0 +1,11 @@
+import { Application, Query } from '@microsoft/teams-ai';
+import { ApplicationTurnState } from '../app';
+import * as userME from './userME';
+
+const setup = (app: Application<ApplicationTurnState>) => {
+
+    app.messageExtensions.query("userQuery", userME.query);
+    
+};
+
+export { setup };
